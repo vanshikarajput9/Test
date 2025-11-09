a71fbb15c43c8b03d9540530d155f93df1406325
VANSHIKA RAJPUT <RAJPUTVANSHIKA815@GMAIL.COM>
2025-11-09 12:52:58 +0530

    Create AboutSection.jsx

---
+++ components/AboutSection.jsx
@@ -0,0 +1,13 @@
+export default function AboutSection() {
+  return (
+    <section id="about" className="about">
+      <div className="container">
+        <h3>About Us</h3>
+        <p>
+          We are a team of passionate developers building intelligent solutions with
+          a focus on usability, innovation, and customer satisfaction.
+        </p>
+      </div>
+    </section>
+  )
+}
