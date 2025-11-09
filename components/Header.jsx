4b9bc040f9521d76e0c1dec5b0213455631511ee
VANSHIKA RAJPUT <RAJPUTVANSHIKA815@GMAIL.COM>
2025-11-09 12:49:04 +0530

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <h1 className="logo">EZ Labs</h1>
+        <nav>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
