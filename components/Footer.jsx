d9c31fa972a65ee55f3837bbdaad5ac3bd11f52b
VANSHIKA RAJPUT <RAJPUTVANSHIKA815@GMAIL.COM>
2025-11-09 12:54:31 +0530

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>© {new Date().getFullYear()} EZ Labs. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
