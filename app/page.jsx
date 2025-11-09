58d22848777aed687bc1ad86b149d5e1418ce675
VANSHIKA RAJPUT <RAJPUTVANSHIKA815@GMAIL.COM>
2025-11-09 12:42:39 +0530

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import HeroSection from '../components/HeroSection'
+import AboutSection from '../components/AboutSection'
+import ContactForm from '../components/ContactForm'
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+      <AboutSection />
+      <ContactForm />
+    </main>
+  )
+}
